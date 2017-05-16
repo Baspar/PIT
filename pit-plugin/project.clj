7c7885f3b2d2ce0792d56b5faecbce443a153ffa
Baspar <Baspar@users.noreply.github.com>
2017-05-16 21:03:50 +0800

    v0.1.7 => v0.1.8

--- pit-plugin/project.clj
+++ pit-plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject pit-plugin "0.1.7"
+(defproject pit-plugin "0.1.8"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
